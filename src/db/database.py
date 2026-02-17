d3d5bac53ba2b6f4c90c437d02c031a720b009fb
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-02-17 22:32:41 +0530

    added database.py

---
+++ src/db/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from utils.globals import DATABASE_URL
+
+# Creates an engine
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Create a session
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+# Base class to create declarative objects(tables)
+Base = DeclarativeBase()
