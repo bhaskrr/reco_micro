af76db17b1e900e83683dc3843469b3fe35d2583
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-02-17 23:14:10 +0530

    fixed timestamp attribute of interaction class

--- src/db/models.py
+++ src/db/models.py
@@ -11,4 +11,4 @@ class Interaction(Base):
     item_id = Column(String, index=True)
     action_type = Column(String)  # 'click', 'like', 'purchase'
     weight = Column(Float, default=1.0)  # For "Weighted Interactions" feature!
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    timestamp = Column(DateTime, default=datetime.UTC)
