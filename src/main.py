8afed04d8e5a5c36912739fb85cf5c7797059da1
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2026-02-18 09:14:25 +0530

    added database tables creation on startup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from src.api.routes import health
 
+from src.db.database import Base, engine
+import src.db.models
+
 app = FastAPI(title="RecoMicro API", version="v1")
+
+Base.metadata.create_all(engine)
 
 app.include_router(prefix="/health", router=health.router)
 
